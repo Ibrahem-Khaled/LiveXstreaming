cdfa9ea68865be4ec78e4da6943cb45aabb05637
Ibrahem-Khaled <xx.vvv48@gmail.com>
2024-04-23 19:45:40 +0200

    add landng page

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,21 @@ use App\Http\Controllers\web\addcategoriesController;
 use App\Http\Controllers\web\addchannelController;
 
 
-Route::get('/', [addcategoriesController::class, 'index'])->name('home');
-Route::post('/post', [addcategoriesController::class, 'create'])->name('createCategories');
-Route::get('/edit/{id}', [addcategoriesController::class, 'show'])->name('showCategories');
-Route::post('/update/{id}', [addcategoriesController::class, 'update'])->name('updateCategories');
-Route::post('/delete/{id}', [addcategoriesController::class, 'destroy'])->name('destroyCategories');
+Route::get('/', function () {
+    return view('home');
+});
 
+Route::group(['prefix' => 'dashboard',], function () {
+    Route::get('/', [addcategoriesController::class, 'index'])->name('home');
+    Route::post('/post', [addcategoriesController::class, 'create'])->name('createCategories');
+    Route::get('/edit/{id}', [addcategoriesController::class, 'show'])->name('showCategories');
+    Route::post('/update/{id}', [addcategoriesController::class, 'update'])->name('updateCategories');
+    Route::post('/delete/{id}', [addcategoriesController::class, 'destroy'])->name('destroyCategories');
 
+    Route::get('/channels/{id}', [addchannelController::class, 'index'])->name('addchannels');
+    Route::post('/create/channels', [addchannelController::class, 'create'])->name('createChannels');
+    Route::get('/channel/only/{id}', [addchannelController::class, 'show'])->name('channel');
+    Route::post('/update/channels/{id}', [addchannelController::class, 'update'])->name('updateChannels');
+    Route::post('/delete/channels/{id}', [addchannelController::class, 'destroy'])->name('deleteChannels');
+});
 
-Route::get('/channels/{id}', [addchannelController::class, 'index'])->name('addchannels');
-Route::post('/create/channels', [addchannelController::class, 'create'])->name('createChannels');
-Route::get('/channel/only/{id}', [addchannelController::class, 'show'])->name('channel');
-Route::post('/update/channels/{id}', [addchannelController::class, 'update'])->name('updateChannels');
-Route::post('/delete/channels/{id}', [addchannelController::class, 'destroy'])->name('deleteChannels');
