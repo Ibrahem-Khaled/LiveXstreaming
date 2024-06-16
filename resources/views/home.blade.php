ec690254d007e651221ed6303fb5824cbdb2fed4
Ibrahem-Khaled <xx.vvv48@gmail.com>
2024-06-16 09:32:40 +0200

    add home page new

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,11 @@
     <link rel="stylesheet"
         href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+
     <style>
         @import url("https://fonts.googleapis.com/css?family=Roboto&display=swap");
 
@@ -37,7 +42,7 @@
         }
 
         .navbar .logo img {
-            width: 150px;
+            width: 50px;
         }
 
         .navbar .buttons {
@@ -47,8 +52,9 @@
             cursor: pointer;
             text-align: center;
             flex: 1;
-            margin: 0 5px;
+            margin: 10px;
             max-width: 200px;
+            align-self: center;
             /* تحديد عرض أقصى لتجنب العرض الزائد */
         }
 
@@ -177,14 +183,28 @@
 
 <body>
 
-    <div class="navbar">
-        <div class="logo">
-            <img src="https://cdn-icons-png.flaticon.com/128/1023/1023521.png" alt="TVTimes Logo">
-        </div>
-        <div class="buttons" id="sidebarToggle">الفئات</div>
-        <div class="buttons">حمل تطبيق الاندرويد من هنا</div>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">TVTimes</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <div class="buttons">حمل تطبيق الاندرويد من هنا</div>
+                </li>
+                <li class="nav-item">
+                    <div class="buttons" id="sidebarToggle">الفئات</div>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">البحث</button>
+            </form>
+        </div>
+    </nav>
     <div class="sidebar" id="sidebar">
         <span class="close-btn" id="closeSidebar">&times;</span>
         <h2>الفئات</h2>
@@ -252,6 +272,15 @@
                 $('#sidebar').removeClass('show');
             });
         });
+    </script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
 </body>
 
